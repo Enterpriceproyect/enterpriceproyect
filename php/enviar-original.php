a8c91b5266e431e74e5269642ac901ba70cba3c8
Jorge <126220892+Enterpriceproyect@users.noreply.github.com>
2023-05-07 00:01:26 -0300

    se agrega php

---
+++ php/enviar-original.php
@@ -0,0 +1,40 @@
+++<?php 
+
+//Bloque de las Variables
+$nya=$_POST['nya'];
+
+$calle=$_POST['calle'];
+
+$phone=$_POST['phone'];
+
+$clave=$_POST['clave'];
+
+$mensaje=$_POST['mensaje'];
+
+
+// Configuramos datos de email
+$destinatario="mimail@hotmail.com";
+
+$asunto= "Nuevo Postulante";
+
+
+// Cuerpo de email
+
+$cuerpo.="Nombres: ".$nya."\n";
+
+$cuerpo.="Domicilio: ".$calle."\n";
+
+$cuerpo.="Teléfono: ".$phone."\n";
+
+$cuerpo.="Contraseña: ".$clave."\n\n";
+
+$cuerpo.="Comentario: ".$mensaje."\n";
+
+
+// enviamos el email
+
+mail($destinatario,$asunto,$cuerpo);
+
+echo $nombre, "Gracias por contactarnos!!!";		
+
+?>
